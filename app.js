c0156e10c830f4d1d520feeb4728c870a83134a1
Max Gavrilov <gavrilovworks@gmail.com>
2018-04-18 19:37:26 +0300

    Совместил сортировку по клику по столбцу и по форме

---
+++ app.js
@@ -0,0 +1,97 @@
+let data = [{
+  id: 1,
+  number: 1,
+  customer: 'Иванова Ирина',
+  manager: 'Петрова Полина',
+  status: 'Выполнен',
+  summ: 1500
+      }, {
+  id: 2,
+  number: 2,
+  customer: 'Сидорова Алла',
+  manager: 'Петрова Полина',
+  status: 'В обработке',
+  summ: 11000
+      }, {
+  id: 3,
+  number: 3,
+  customer: 'Лукьянова Прасковья',
+  manager: 'Смирнов Андрей',
+  status: 'Отказ',
+  summ: 15020
+      }, {
+  id: 4,
+  number: 4,
+  customer: 'Гришин Сидор',
+  manager: 'Смирнов Андрей',
+  status: 'Выполнен',
+  summ: 4500
+    }];
+
+let type = [{
+  sortCol: 'number'
+                 }, {
+  sortCol: 'customer'
+                 }, {
+  sortCol: 'manager'
+                 }, {
+  sortCol: 'status'
+                 }, {
+  sortCol: 'summ'
+                 }]
+
+var app = angular.module('sortApp', [])
+app.controller('sortController', ['$http', function ($http) {
+  var sort = this;
+
+  sort.orders = data;
+  sort.order = undefined;
+
+  sort.colType = type;
+
+  sort.sortType = 'number';
+
+  sort.sortOrder = false;
+
+  sort.openOrder = function (order) {
+    sort.order = order;
+    console.log("123");
+  };
+  //Кнопка отмены
+  sort.cancel = function () {
+    sort.order = undefined;
+  };
+  //Кнопка Добавить
+  sort.add = function () {
+    let order = {};
+    //sort.orders.push(order);
+    sort.order = order;
+    if (sort.orders.id > 0) {
+      console.log("111");
+
+      sort.addBtn = "Изменить";
+    } else {
+      sort.addBtn = "Добавить";
+    }
+  };
+  //Кнопка удалить
+  sort.remove = function (order) {
+    let index = sort.orders.indexOf(order);
+    if (index > -1) {
+      sort.orders.splice(index, 1);
+    }
+    console.log(order);
+  };
+
+  //Появление Input панели в Сумме
+  sort.summInputHide = false;
+  sort.summInput = function () {
+    console.log("123");
+    sort.summInputHide = true;
+  };
+  //Кнопка добавить/Изменить
+  sort.addBtn = "Change";
+
+
+
+    }]);
